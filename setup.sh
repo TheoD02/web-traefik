148d99230dc5005aecfdbdbac57c927f930d30ae
Theo D <theo.d02290@gmail.com>
2023-12-18 22:47:39 +0100

    Setup to use *.localhost instead of remote host

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo apt install mkcert
-mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "alls.dev" "*.alls.dev"
+mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "web.localhost" "*.web.localhost" "db.localhost" "*.db.localhost"
 certutil.exe -addstore -user "Root" "\home\t\.local\share\mkcert\rootCA.pem"
