b5d5aa1cec3d82f60d02b572da000922edefe8f6
Theo D <theo.dulieu@wyzgroup.com>
2024-03-03 16:32:46 +0100

    Update Traefik configuration and setup script
    Added 'network' to traefik.yml and updated setup.sh to handle certificates on both WSL and non-WSL systems. Also updated compose.yaml to use 'network_mode: host' instead of manually specifying ports and networks.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+
 sudo apt install mkcert
 mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "web.localhost" "*.web.localhost" "db.localhost" "*.db.localhost"
-certutil.exe -addstore -user "Root" "\home\t\.local\share\mkcert\rootCA.pem"
+
+isWsl=$(uname -a | grep -q Microsoft && echo "true" || echo "false")
+
+if [ $isWsl = "true" ]; then
+    certutil.exe -addstore -user "Root" "$HOME/.local/share/mkcert/rootCA.pem"
+else
+    sudo cp "$HOME/.local/share/mkcert/rootCA.pem" /usr/local/share/ca-certificates/
+    sudo update-ca-certificates
+fi
