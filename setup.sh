8317bbbf867d64ca26ea1a4fb38471af62b527e2
Theo D <theo.d02290@gmail.com>
2024-03-08 19:51:37 +0100

    Fix Ubuntu 22.04 setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt install mkcert
+sudo apt install mkcert libnss3-tools
+mkcert -install
 mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "web.localhost" "*.web.localhost" "db.localhost" "*.db.localhost"
 
 isWsl=$(uname -a | grep -q Microsoft && echo "true" || echo "false")
