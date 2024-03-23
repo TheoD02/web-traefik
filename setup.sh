6021a6997a3419cae9c2a88e25f960cbdaa24afc
Theo D <theo.d02290@gmail.com>
2024-03-23 17:44:40 +0100

    fix: WSL Detection, and compose ports rollback instead of network_mode: host

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ sudo apt install mkcert libnss3-tools
 mkcert -install
 mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "web.localhost" "*.web.localhost" "db.localhost" "*.db.localhost"
 
-isWsl=$(uname -a | grep -q Microsoft && echo "true" || echo "false")
+isWsl=$(uname -a | grep -q -i Microsoft && echo "true" || echo "false")
 
 if [ $isWsl = "true" ]; then
     certutil.exe -addstore -user "Root" "$HOME/.local/share/mkcert/rootCA.pem"
-else
-    sudo cp "$HOME/.local/share/mkcert/rootCA.pem" /usr/local/share/ca-certificates/
-    sudo update-ca-certificates
 fi
+
+sudo cp "$HOME/.local/share/mkcert/rootCA.pem" /usr/local/share/ca-certificates/
+sudo update-ca-certificates
